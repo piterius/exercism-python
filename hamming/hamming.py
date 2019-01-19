f84d659e6f737abcb21e170b5d7a2f0317bd532e
piterius <edgar.pilch@protonmail.ch>
2019-01-19 20:48:02 +0100

    Solution for hamming exercise.

---
+++ hamming/hamming.py
@@ -0,0 +1,5 @@
+def distance2(first, second):
+    if len(first) != len(second):
+        raise ValueError("Not the same length")         #raising error
+    else:
+        return sum(1 for i in range(len(first)) if first[i] != second[i])
