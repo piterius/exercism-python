051b621f1f68d5104d48a407e3e6280181924833
piterius <edgar.pilch@protonmail.ch>
2019-02-20 14:06:59 +0100

    removal of redundancy

--- grains/grains.py
+++ grains/grains.py
@@ -1,10 +1,10 @@
 def on_square(number):
-    if number not in range(1, 65, 1):
+    if number not in range(1, 65):
         raise ValueError("Wrong number, it should be integer from 1 to 64.")
     return 2 ** (number - 1)
 
 
 def total_after(number):
-    if number not in range(1, 65, 1):
+    if number not in range(1, 65):
         raise ValueError("Wrong number, it should be integer from 1 to 64.")
     return sum((2 ** place for place in range(number)))
