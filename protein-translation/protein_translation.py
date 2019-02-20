c77366319627fbd5c014264bb9f0310919513583
piterius <edgar.pilch@protonmail.ch>
2019-02-20 14:20:33 +0100

    adding capittal letters for constant

--- protein-translation/protein_translation.py
+++ protein-translation/protein_translation.py
@@ -1,4 +1,4 @@
-codons = {"AUG": "Methionine", "UUU": "Phenylalanine", "UUC": "Phenylalanine", "UUA": "Leucine", "UUG": "Leucine",
+CODONS = {"AUG": "Methionine", "UUU": "Phenylalanine", "UUC": "Phenylalanine", "UUA": "Leucine", "UUG": "Leucine",
           "UCU": "Serine", "UCC": "Serine", "UCA": "Serine", "UCG": "Serine", "UAU": "Tyrosine", "UAC": "Tyrosine",
           "UGU": "Cysteine", "UGC": "Cysteine", "UGG": "Tryptophan", "UAA": "STOP", "UAG": "STOP", "UGA": "STOP"}
 
@@ -6,11 +6,10 @@ codons = {"AUG": "Methionine", "UUU": "Phenylalanine", "UUC": "Phenylalanine", "
 def proteins(strand):
     result = []
     for i in range(0, len(strand), 3):
-        if strand[i:i + 3] not in codons:
+        if strand[i:i + 3] not in CODONS:
             raise ValueError("Incorrect strand")
-        if codons[strand[i:i + 3]] == "STOP":
+        if CODONS[strand[i:i + 3]] == "STOP":
             return result
         else:
-            result.append(codons[strand[i:i + 3]])
+            result.append(CODONS[strand[i:i + 3]])
     return result
-
