c88bb083f84c8ee7dbb37b232553f6a257323e99
piterius <edgar.pilch@protonmail.ch>
2019-02-27 18:01:29 +0100

    added solution for allergies task

---
+++ allergies/allergies.py
@@ -0,0 +1,10 @@
+ALLERGENS = {1: "eggs", 2: "peanuts", 4: "shellfish", 8: "strawberries", 16: "tomatoes", 32: "chocolate", 64: "pollen",
+             128: "cats"}
+
+
+class Allergies:
+    def __init__(self, score):
+        self.lst = [ALLERGENS[i] for i in ALLERGENS.keys() if i & score]
+
+    def is_allergic_to(self, product):
+        return product in self.lst
